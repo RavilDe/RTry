63fce3c0be381c8839390861bd9c8c496d1ebb15
realstarosta <realstarosta@gmail.com>
2016-11-17 17:13:40 +0300

    formatting

--- Rrrrrr.R
+++ Rrrrrr.R
@@ -4,12 +4,12 @@ help.search("sin")
 
 #### Урок 1.2.8
 2+1
-sin(pi/3)
-sin(pi/2)
+sin(pi / 3)
+sin(pi / 2)
 
 cats <- 3
 dogs <- 4
-pets <- cats+dogs
+pets <- cats + dogs
 
 ?rnorm # нормальное распределение
 rnorm(15)
@@ -25,18 +25,18 @@ return_two()
 
 # Окружение функции
 addten <- function(x) {
-  x <- x+10
+  x <- x + 10
 }
 addten(cats)  # не работает, т.к. мы переприсвоили Х в локальном окружении, а вглобальном он так и остался
 
 addten <- function(x) {
-  return(x+10)
+  return(x + 10)
 }
 addten(cats)  # вот теперь все верно
 
 # Глобальное присваивание (лучше не использовать)
 addten <- function(x) {
-  moredogs <<- (x+10)
+  moredogs <<- (x + 10)
 }
 addten(dogs); dogs; moredogs
 # Функции меняющие глобальные переменные - это функции со сторонним эффектом (side effect)
@@ -124,75 +124,75 @@ matrix(1:6, ncol = 3, byrow = TRUE)
 
 m <- matrix(1:6, ncol = 3)
 dim(m)
-c(nrow(m),ncol(m))
+c(nrow(m), ncol(m))
 dim(m) <- NULL; m
-dim(m) <- c(2,3); m
+dim(m) <- c(2, 3); m
 
 m1 <- matrix(1:4, nrow = 2)
-m2 <- matrix(c(1,2,2,3), nrow = 2)
+m2 <- matrix(c(1, 2, 2, 3), nrow = 2)
 
-m1+m2   # операции производятся поэлементно
-m1+5
-m1*2
-m1*m2
+m1 + m2   # операции производятся поэлементно
+m1 + 5
+m1 * 2
+m1 * m2
 
 m1 %*% m2  # умножение по правилам линейной алгебры
 
 #  Индексирование матриц
 m <- matrix(1:10, ncol = 5)
-m[1,3]
-m[2,]
-m[,4]
+m[1, 3]
+m[2, ]
+m[ ,4]
 
-m[1,] <- 0; m
-m[,-5] <- 11:18; m
+m[1, ] <- 0; m
+m[ , -5] <- 11:18; m
 
 # Схлопывание размерности
 m <- matrix(1:10, ncol = 5)
-ind <- c(1,3,5)
-m[,ind]
+ind <- c(1, 3, 5)
+m[ , ind]
 
 ind <- 3
-m[,ind]
-m[,ind, drop=FALSE]   # drop - чтобы столбец не превратился в вектор и оставался столбцом
+m[ , ind]
+m[ , ind, drop=FALSE]   # drop - чтобы столбец не превратился в вектор и оставался столбцом
 # TRUE и FALSE - T и F
 # Именованые матрицы
 m <- matrix(1:10, ncol = 5)
-rownames(m) <- c("row1","row2")
-colnames(m) <- paste0("column",1:5)
-m["row2",c("column2","column4"), drop=F]
+rownames(m) <- c("row1", "row2")
+colnames(m) <- paste0("column", 1:5)
+m["row2", c("column2", "column4"), drop=F]
 
 # Присоединение матриц: rbind / cbind
-rbind(m1,m2)  # сложение по вертикали
-cbind(m1,m2)  # сложение по горизонтали
+rbind(m1, m2)  # сложение по вертикали
+cbind(m1, m2)  # сложение по горизонтали
 
 # Аргумент ... (elipsis)
 # позволяет передавать неограниченное кол-во элементов 
-cbind(m1,m2,1:2,c(5,3),m2[,1],m1*3,cbind(m2,m1))
+cbind(m1, m2, 1:2, c(5,3), m2[ , 1], m1 * 3, cbind(m2, m1))
 
 # Применение функции к матрице: apply
-m <- matrix(1:25,5)
-f <- function(x) sum(x^2)
+m <- matrix(1:25, 5)
+f <- function(x) sum(x ^ 2)
 
 # три аргумента функции apply:
 # - матрица
 # - индекс (1 - по строкам, 2 - по столбцам)
 # - функция
-apply(m,1,f)
-apply(m,2,f)
+apply(m, 1, f)
+apply(m, 2, f)
 
-apply(m,1:2, function(i) if (i>13) i else 13) # 1:2 - обращение ко всей матрице
+apply(m, 1:2, function(i) if (i > 13) i else 13) # 1:2 - обращение ко всей матрице
 # функция без имени - анонимная функция
-m[m<=13] <- 13; m # но так проще
+m[m <= 13] <- 13; m # но так проще
 
 # rowSums, rowMeans, colSums, colMeans
 # сумма и среднее
-m <- matrix(1:25,5)
+m <- matrix(1:25, 5)
 rowSums(m)
 # проверим
-all.equal(rowSums(m),apply(m,1,sum))
-all.equal(rowMeans(m),apply(m,1,mean))
+all.equal(rowSums(m), apply(m, 1, sum))
+all.equal(rowMeans(m), apply(m, 1, mean))
 # задача 2.1.3
-mat <- matrix(0,3,2)
-mat[3,2]
-mat>5
+mat <- matrix(0, 3, 2)
+mat[3, 2]
+mat > 5
