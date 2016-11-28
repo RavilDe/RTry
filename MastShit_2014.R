f19dfcea1e0363f9208070a2d978ed54bf2c0e86
realstarosta <realstarosta@gmail.com>
2016-11-28 11:21:58 +0300

    1 chapter

--- MastShit_2014.R
+++ MastShit_2014.R
@@ -1,1 +1,25 @@
 # Мастецкий, Шитиков
+
+?help.start
+
+installed.packages(priority = "base") # базовые пакеты
+installed.packages(priority = "recommended") # базовые пакеты
+
+# копируем в буфер обмена список пакетов
+packlist <- rownames(installed.packages())
+write.table(packlist, "clipboard", sep = "\t", col.names = NA)
+
+args(write.csv) # выводит аргументы функции
+
+# ввод функции без скобок приводит к выводу исходного текста функции 
+args 
+sin
+IQR
+
+# вывод всех методов этой функции
+methods("predict")
+methods("as")
+methods("plot")
+methods("print")
+methods("is")
+
