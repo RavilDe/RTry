1224004a277051a8736d0c6f33dbe2bb80a4bf95
realstarosta <realstarosta@gmail.com>
2016-12-06 00:32:31 +0300

    Swirl_lessons 5->7

--- Swirl_lessons.R
+++ Swirl_lessons.R
@@ -100,6 +100,78 @@ paste("Hello", "world!", sep = " ")
 paste(1:3, c("X", "Y", "Z"), sep = "")
 paste(LETTERS, 1:4, sep = "-")
 
+### 5
+### Missing Values
+x <- c(44, NA, 5, NA)
+x * 3
+y <- rnorm(1000) # 1000 рандомных чисел
+z <- rep(NA, 1000) # 1000 NA-шек
+my_data <- sample(c(y, z), 100) # случайная выборка 100 чисел из 2000 смешанных чисел и NAшек
+my_na <- is.na(my_data)
+my_na # получаем смесь из 100 TRUE и FALSE
 
+my_data == NA # в ответ получаем 100 NA
+# The reason you got a vector of all NAs is that NA is not really a value, but just a placeholder for a quantity that is not available.
+# Therefore the logical expression is incomplete and R has no choice but to return a vector of the same length as my_data that contains all NAs.
 
+sum(my_na)
+my_data
 
+0/0
+Inf - Inf
+### 6
+### Subsetting Vectors
+
+x
+x[is.na(x)] # выводит вектор со всеми NA
+y <- x[!is.na(x)] # выводит вектор со НЕ NA 
+y
+y[y > 0] # векторвсех положительных чисел в векторе Y
+x[x > 0] # выводит все положительные НО с NA
+x[!is.na(x) & x > 0] # а вот это уже выдаст то что мы хотели
+
+x[c(3, 5, 7)]
+x[0]
+x[3000]
+
+x[c(-2, -10)]
+x[-c(2, 10)] # тоже самое и еороче
+
+# именованые вектора
+vect <- c(foo = 11, bar = 2, norf = NA)
+vect
+names(vect)
+vect2 <- c(11, 2, NA)
+names(vect2) <- c("foo", "bar", "norf")
+identical(vect, vect2)
+
+vect["bar"]
+vect[c("foo", "bar")]
+
+### 7
+### Matrices and Data Frames
+
+my_vector <- 1:20
+my_vector
+dim(my_vector) # т.к. это пока вектор, у него нет dimensions
+length(my_vector) # а вот длина есть
+dim(my_vector) <- c(4, 5)
+dim(my_vector) # оп! появилась матрица; мы присвоиили атрибуту dim нашего вектора размерность 4х5 и вектор преобразовался в матрицу
+attributes(my_vector)
+my_vector
+class(my_vector) # выдает нам класс объекта
+
+my_matrix <- my_vector
+my_matrix2 <- matrix(1:20, nrow = 4, ncol = 5) 
+identical(my_matrix, my_matrix2)
+
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+cbind(patients, my_matrix)
+
+my_data <- data.frame(patients, my_matrix)
+my_data
+class(my_data)
+
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+colnames(my_data) <- cnames
+my_data
