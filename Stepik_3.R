4bc6b4c6a17e4da9e9926216916f0f8cc280f9bc
realstarosta <realstarosta@gmail.com>
2017-01-18 00:53:18 +0300

    Stepik_3 - 1.2.2 full

--- Stepik_3.R
+++ Stepik_3.R
@@ -9,3 +9,32 @@ library(ggplot2)
 data(diamonds) # загрузка (показ) данных в окружение в виде обещания
 str(diamonds)  # обещание выполнено :)
 
+# немного тренировки и проверки
+i <- 1
+diamonds[i, 8:10]
+min(diamonds[i, 8:10])
+
+# неправильный код ))
+min_size <- c()
+system.time({ for (i in 1:nrow(diamonds)) {
+  min_size <- c(min_size, min(diamonds[i, 8:10]))
+}
+}) # почти 50 секунд на маке (при работающем FAH :)
+
+head(min_size)
+
+# вторая попытка
+min_size <- numeric(nrow(diamonds))
+system.time({ for (i in 1:nrow(diamonds)) {
+  min_size[i] <- min(diamonds[i, 8:10])
+}
+}) # 21 секунда на маке (при работающем FAH :)
+
+# а теперь быстрый вариант
+system.time(min_size2 <- apply(diamonds[, 8:10], 1, min))
+  # работаем только с 8-10 столбцами датафрейма
+  # 1 - применяем функцию к строкам (2 - к столбцам)
+  # min - применяемая функция
+  ### расчет занял 0,414 секунды :)
+
+
