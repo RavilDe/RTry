4a8624e97fef7082672c1dec762462573a9fd744
realstarosta <realstarosta@gmail.com>
2017-01-19 18:17:12 +0300

    Stepik_3 1.3.5 - 1.4.4

--- Stepik_3.R
+++ Stepik_3.R
@@ -136,9 +136,18 @@ apply(d, 2, function(x) x[x < 0 & is.na(x) == F])
 ### 1.3.5
 d[4, 1] <- NA
 
-df <- as.data.frame(list(V1 = c(NA, -0.5, -0.7, -8), V2 = c(-0.3, NA, -2, -1.2), V3 = c(1, 2, 3, NA)))
+df <- as.data.frame(list(V1 = c(NA, -0.5, -0.7, -8), 
+                         V2 = c(-0.3, NA, -2, -1.2), 
+                         V3 = c(1, 2, 3, NA)))
+# свое с подсказками
+df1 <- apply(df, 2, function(x) as.numeric(x[x < 0 & is.na(x) == F]))
+df1[sapply(df1, length)>0]
 
-df1 <- apply(df, function(x) x[x < 0 & is.na(x) == F])
+# чужое и получше (без sapply)
+get_negative_values <- function(test_data) {
+  cols <- apply(test_data, 2 ,function(x) any(x < 0, na.rm = TRUE))
+  apply(test_data[cols], 2, function(x) x[!is.na(x) & x < 0])
+}
 
 ### 1.3.6
 head(iris)
@@ -146,5 +155,78 @@ str(iris)
 aov(Sepal.Length ~ Species, iris) # дисперсионный анализ по первому столбцу
 
 # по всем количественным переменным
-apply(iris[, 1:4], 2, function(x) aov(x ~ Species, iris))
+aov_result <- apply(iris[, 1:4], 2, function(x) aov(x ~ Species, iris))
+norm_test <- apply(iris[, 1:4], 2, function(x) shapiro.test(x))
+norm_test_p <- apply(iris[, 1:4], 2, 
+                   function(x) shapiro.test(x)$p.value)
 
+str(aov_result)
+norm_test$Sepal.Length
+
+### 1.3.8
+test_data <- as.data.frame(list(V1 = c(NA, NA, NA, NA, 13, 12, 9, 10, 8, 9, 11, 
+                                       11, 10, 12, 9), 
+                                V2 = c(NA, 12, 8, NA, 11, 11, 9, 8, 8, 10, 10, 
+                                       11, 10, 10, 10), 
+                                V3 = c(NA, 5, NA, 13, 12, 11, 11, 14, 8, 12, 8, 
+                                       8, 10, 10, 8), 
+                                V4 = c(10, 10, 10, 10, 13, 10, 11, 7, 12, 10, 
+                                       7, 10, 13, 10, 9)))
+# меняем NA на среднее в столбце
+na_rm <- function(x) {
+  as.data.frame(apply(test_data, 2, 
+                      function(x) ifelse(is.na(x), mean(x, na.rm = T), x)))
+}
+
+na_rm(test_data)
+
+mean(test_data$V3, na.rm = T)
+
+#*******************************************************************************
+### 1.4 Функции семейства apply. Часть 3
+### 1.4.2
+# про apply:
+# - только для data.frame или матрицы
+# - НЕЛЬЗЯ применить для вектора или списка
+
+my_list <- list(x = c(rnorm(30), NA), y = rnorm(10))
+str(my_list)
+
+lapply(my_list, mean)
+lapply(my_list, mean, na.rm = T)
+lapply(my_list, function(x) x * 2)
+
+# про lapply
+# - на входе и на выходе - список
+# - проброс аргументов
+# - безымянные функции
+
+sapply(my_list, mean, na.rm = T) # ветор
+# отличие sapply от lapply
+# s(simpify) выдает вектор или матрицу;
+# и только в исключительных случаях выдает список (list)
+
+sapply(my_list, range, na.rm = T) # матрица
+sapply(my_list, range, na.rm = T, simplify = F) # превращается в lapply
+
+### 1.4.3
+d <- data.frame(X1 = c(-1, -2, 0), X2 = c(10, 4, NA), X3 = c(-4, NA, NA))
+positive_sum <- function(a) {
+  lapply(a, function(x) sum(x[x>0], na.rm = T))
+}
+
+positive_sum(d)
+
+### 1.4.4
+cars <- c("Mazda", "Volga", "Mercedes")
+car <- "Mazda RX4"
+
+grepl("Mazda", "Mazda RX4") # первый элемент содержится во втором
+
+# через цикл
+for (i in cars) {
+  print(grepl(i, car))
+}
+
+sapply(cars, function(x) grepl(x, car))
+attributes(cars)
