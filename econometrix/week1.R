11f692fccc355d2faf8949e5c92beac3a7c7de9f
realstarosta <realstarosta@gmail.com>
2017-05-18 14:25:13 +0300

    ecom=nometrix - test week 1 (take 1)

--- econometrix/week1.R
+++ econometrix/week1.R
@@ -150,8 +150,149 @@ qplot(data = d2, speed, dist) +
 t <- swiss
 ?swiss
 glimpse(t)
-desc
+describe(t)
 
+# Диаграммы рассеивания
+ggpairs(t)
+model2 <- lm(data = t,
+             Fertility ~ Agriculture + Education + Catholic)
+coef(model2)
+fitted(model2) # спрогнозированные значения
+residuals(model2) 
 
+report <- summary(model2)
+report$r.squared
 
+cor(t$Fertility, fitted(model2)) # корреляция м/у исходной и спрогнозированной
+cor(t$Fertility, fitted(model2))^2 # R^2
 
+# Новый набор данны
+nd2 <- data.frame(Agriculture = 0.5,
+                  Catholic = 0.5,
+                  Education = 20)
+
+# Прогонозируем новый набор данных по модели 2
+predict(model2, nd2) # спрогнозированный показатель фертильности
+
+### Test Week 1
+# 1
+weighted.mean(c(-1, 0, 1), c(0.3, 0.2, 0.5))
+
+# 2
+  # 7 - неа
+  # E(aX+bY+c)=aE(X)+bE(Y)+c  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# 3
+  # 22 - неа
+  # Var(aX+bY+c)=a2Var(X)+b2Var(Y)+2abCov(X,Y) %%%%%%%%%%%%%%%%%%%%%
+
+# 4
+  # Прогнозирование заработной платы выпускника ВУЗа
+  # Установление направления причинно-следственной связи (определяет ли
+  # образование продолжительность жизни или наоборот)
+  #  - все наоборот((((( %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# 5
+  # Отклонений реальных данных от предсказаний по модели
+
+# 6
+  # Одна зависимая переменная объясняется при помощи набора независимых
+
+# 7
+  # RSS не верно%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# 8
+checkSS <- function(TSS, RSS, ESS, R2) {
+  if (TSS == RSS + ESS & R2 == ESS / TSS) {
+    print("Подходит")
+  } else {
+    print("Не подходит")
+  }
+}
+checkSS(20, 8, 12, 0.6) # только этот подходит
+checkSS(10, 8, 4, 0.4)
+checkSS(20, 8, 6, 0.6)
+checkSS(20, 9, 12, 0.6)
+
+# 9
+  # Точность "подгонки" модели к данным
+
+# 10
+TSS <- 100
+RSS <- 70
+R2 <- (TSS - RSS) / TSS # 0.3
+
+# 11
+R2 <- 0.7
+TSS <- 130
+RSS <- TSS - R2 * TSS # 39
+
+# 12
+  # 0 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# 13
+y <- c(2, 2, 1.75, 2.25, 2)
+y_hat <- 2
+TSS <- sum((y - mean(y))^2)
+ESS <- sum((y_hat - mean(y))^2)
+R2 <- ESS / TSS
+
+# 14
+data(sleep)
+str(sleep)
+sleep[7, 1] # 3.7
+
+# 15
+data(sleep)
+a <- mean(sleep$extra)^2 # 2.37
+round(a, 2)
+
+# 16
+data(sleep)
+min(sleep$extra) + max(sleep$extra)
+
+# 17
+data(sleep)
+mean((sleep[1:10, 1] - mean(sleep[1:10, 1]))^2)
+var(sleep[1:10, 1]) # variaty или дисперсия случайной величины 
+sd(sleep[1:10, 1])^2
+
+# 18
+data(mtcars)
+str(mtcars)
+model <- lm(data = mtcars, mpg ~ disp + hp + wt)
+coef(model)
+
+y <- mtcars$mpg
+y_hat <- fitted(model)
+cor(y, y_hat)^2 # выборочная корреляция в квадрате = R2
+
+RSS <- deviance(model)
+TSS <- sum((y - mean(y)) ^ 2)
+ESS <- TSS - RSS
+R2 <- ESS / TSS
+R2
+
+# 19
+data(mtcars)
+str(mtcars)
+model <- lm(data = mtcars, mpg ~ disp + hp + wt + am)
+coef(model)
+
+# 20
+data(mtcars)
+model_1 <- lm(data = mtcars, mpg ~ disp + hp + wt)
+model_2 <- lm(data = mtcars, mpg ~ cyl + hp + wt)
+model_3 <- lm(data = mtcars, mpg ~ disp + cyl + wt)
+model_4 <- lm(data = mtcars, mpg ~ disp + hp + cyl)
+
+r_from_model <- function(model) {
+  y <- mtcars$mpg
+  y_hat <- fitted(model)
+  cor(y, y_hat)^2
+}
+
+r_from_model(model_1)
+r_from_model(model_2)
+r_from_model(model_3)
+r_from_model(model_4)
