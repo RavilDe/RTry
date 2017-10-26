7119d09c246636a4d4b747de138c8150d84755f7
realstarosta <realstarosta@gmail.com>
2017-10-26 20:08:01 +0300

    oes dv some change for mac/linux

--- other/oes_dv.R
+++ other/oes_dv.R
@@ -4,7 +4,8 @@ library(readxl)
 library(dplyr)
 
 # путь к файлу с таблицей
-path <- "~/Desktop/свод СФ.xlsx"
+# path <- "~/Desktop/свод СФ.xlsx" # mac
+path <- "~/Рабочий стол/свод СФ.xlsx" # ubuntu
 
 # загружаем таблицу в гряззном варианте
 df_raw <- read_excel(path, 
@@ -30,22 +31,28 @@ df <- df_raw %>%
             id_app)
 
 # путь к папке с файлами
-path_files <- dir(path = "~/Desktop/ДВ для отправки", full.names = T)
+# path_files <- dir(path = "~/Desktop/ДВ для отправки", full.names = T)
+path_files <- dir(path = "~/Рабочий стол/ДВ для отправки", full.names = T)
 
 # создаем папки с именами из покупателя и номера договора
 for (i in 1:nrow(df)) {
-  new_dir <- paste0("~/Desktop/ДВ по папкам/", df$buyer[i], " ", df$contract[i])
+  # new_dir <- paste0("~/Desktop/ДВ по папкам/", df$buyer[i], " ", df$contract[i])
+  new_dir <- paste0("~/Рабочий стол/ДВ по папкам/", df$buyer[i], " ", df$contract[i])
   dir.create(new_dir)
 }
 
 # раскладываем по этим папкам сф и апп
 # по частям ибо, если запулить все - комп вешается наглухо!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# хм, а на убунте все моментально сработало
 for (i in 1:nrow(df)) {
-  new_dir <- paste0("~/Desktop/ДВ по папкам/", df$buyer[i], " ", df$contract[i])
-  file.copy(from = paste0("~/Desktop/ДВ для отправки/", df$bill[i], "_", df$id_bill[i],".xlsx"),
-            to = paste0(new_dir,"/" ,df$bill[i], "_", df$id_bill[i], ".xlsx")
+  # new_dir <- paste0("~/Desktop/ДВ по папкам/", df$buyer[1], " ", df$contract[1])
+  new_dir <- paste0("~/Рабочий стол/ДВ по папкам/", df$buyer[1], " ", df$contract[1])
+  # file.copy(from = paste0("~/Desktop/ДВ для отправки/", df$bill[1], "_", df$id_bill[1],".xlsx"),
+  file.copy(from = paste0("~/Рабочий стол/ДВ для отправки/", df$bill[1], "_", df$id_bill[1],".xlsx"),
+            to = paste0(new_dir,"/" ,df$bill[1], "_", df$id_bill[1], ".xlsx")
   )
-  file.copy(from = paste0("~/Desktop/ДВ для отправки/", df$app[i], "_", df$id_app[i],".xlsx"),
+  # file.copy(from = paste0("~/Desktop/ДВ для отправки/", df$app[i], "_", df$id_app[i],".xlsx"),
+  file.copy(from = paste0("~/Рабочий стол/ДВ для отправки/", df$app[i], "_", df$id_app[i],".xlsx"),
             to = paste0(new_dir,"/" ,df$app[i], "_", df$id_app[i], ".xlsx")
   )
 }
